7c72d29d88be6ac58d231b2c46393c6c18e53108
David Deaton <dideater@gmail.com>
2021-05-08 10:00:12 -0700

    day 3 update

--- shoppies/src/App.js
+++ shoppies/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import Search from './components/addSearch';
+import Search from './components/addSearch';
 import movies from "./omdb";
 import DisplayMovie from './components/displayMovies';
 
@@ -8,56 +8,76 @@ import DisplayMovie from './components/displayMovies';
 function App() {
 
   // const [search, setSearch] = useState("");
-  // const [poster, setPoster] = useState("");
+  const [poster, setPoster] = useState("");
   const [movieList, setMovieList] = useState([]);
-  // const [title, setTitle] = useState("");
-  // const [year, setYear] = useState("");
+  const [title, setTitle] = useState("");
+  const [year, setYear] = useState("");
 
-  // useEffect(() => {
+  useEffect(() => {
 
-    // const random1 = Math.floor(Math.random() * 12);
+    const random1 = Math.floor(Math.random() * 120);
+    const random2 = Math.floor(Math.random() * 120);
+    const random3 = Math.floor(Math.random() * 120);
+    const random4 = Math.floor(Math.random() * 120);
+    const random5 = Math.floor(Math.random() * 120);
+    const random6 = Math.floor(Math.random() * 120);
+    const random7 = Math.floor(Math.random() * 120);
+    const random8 = Math.floor(Math.random() * 120);
+    const random9 = Math.floor(Math.random() * 120);
 
-    // movies.getMovies().then((response) => {
-    //   console.log("get movies", response);
-    //   setMovieList([...movieList, 
-    //     response[random1].data.Search[0],
-    //     response[(random1+12)].data.Search[0],
-    //     response[(random1+24)].data.Search[0],
-    //     response[(random1+36)].data.Search[0],
-    //     response[(random1+48)].data.Search[0],
-    //     response[(random1+60)].data.Search[0],
-    //     response[(random1+72)].data.Search[0],
-    //     response[(random1+84)].data.Search[0],
-    //     response[(random1+96)].data.Search[0],
-    //     response[(random1+108)].data.Search[0]
-    //   ]);
+    movies.getMovies().then((response) => {
+      console.log("get movies", response);
+      setMovieList([...movieList, 
+        response[random1].data.Search[0],
+        response[random2].data.Search[0],
+        response[random3].data.Search[0],
+        response[random4].data.Search[0],
+        response[random5].data.Search[0],
+        response[random6].data.Search[0],
+        response[random7].data.Search[0],
+        // response[random8].data.Search[0],
+        response[random9].data.Search[0]
+      ]);
+      setPoster(response[random1].data.Search[0].Poster);
+      setTitle(response[random1].data.Search[0].Title);
+      setYear(response[random1].data.Search[0].Year);
 
 
 
-    // })
+    })
 
-    // console.log(movieList);
+    console.log(movieList);
 
 
-  // }, []);
+  }, []);
 
   return (
     <div className="App">
-      <h1>The Shoppies</h1>
-      {/* <Search onAdd={setSearch} /> */}
-      <div className = "movieSuggestions">
-      {/* <DisplayMovie src={poster} title={title} year={year}/>         */}
-      {/* {movieList.map((item, index) => {
-        return (
-          <div className="spot" key={index}> 
-            <img className="searchIcon" src={item.Poster} alt={item.Title} /> 
-            <h3>{item.Title}</h3>
-            <h4>{item.Year}</h4>
-          </div>
-        );
-      })} */}
+      <h1 className="mainTitle">The Shoppies</h1>
+      <div className="section">
+        <h1>Shoppie Nominees</h1>
       </div>
-
+      <div className="joinSection">
+        <div className="section">
+          <h1>Shoppie Search</h1>
+          <Search />
+          <DisplayMovie src={poster} title={title} year={year}/>
+        </div>
+        <div className="section suggestions"> 
+          <h1>Shoppie Suggestions</h1>  
+          <div className = "movieSuggestions">
+            {movieList.map((item, index) => {
+              return (
+                <div className="spot" key={index}> 
+                  <img className="searchIcon" src={item.Poster} alt={item.Title} /> 
+                  <h3>{item.Title}</h3>
+                  <h4>{item.Year}</h4>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- shoppies/src/components/displayMovies.js
+++ shoppies/src/components/displayMovies.js
@@ -1,11 +1,14 @@
+import Button from './Button';
+
 const DisplayMovie = ( {src, title, year} ) => {
 
 
     return (
-        <div className = "spot">        
-            <img className="searchIcon" src={src} alt={title} />
-            <h3>{title}</h3>
-            <h4>({year})</h4> 
+        <div className = "container">        
+            <img src={src} alt={title} />
+            <h2>{title}</h2>
+            <h3>({year})</h3>
+            <Button />
       </div>
     )
 }
